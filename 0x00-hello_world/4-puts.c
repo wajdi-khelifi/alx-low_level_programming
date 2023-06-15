ee5f7b73a08eece184ed2e01f84bb28452853541
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-06-15 14:14:54 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(){
 puts("Programming is like building a multilingual puzzle");
 return (0);
