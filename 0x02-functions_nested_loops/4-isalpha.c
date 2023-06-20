7ccac3f158d1cd225a6526e3814cdba0f2dc6f09
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-06-20 16:02:34 +0000

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha - Checks if a character is alphabet.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is lettre, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+if (isalpha(c))
+{
+return (1);
+}
+else
+return (0);
+}
