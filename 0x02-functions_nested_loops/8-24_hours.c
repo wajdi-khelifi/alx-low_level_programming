5a2a6618eaf42413da2cb4f6c726c8fee2bbd3c4
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-06-20 22:35:39 +0000

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,11 +8,11 @@ int minute, hour;
 for (hour = 0;  hour < 24; hour++)
 for (minute = 0; minute < 60; minute++)
 {
-putchar(hour / 10 + '0');
-putchar(hour % 10 + '0');
-putchar(':');
-putchar(minute / 10 + '0');
-putchar(minute % 10 + '0');
-putchar('\n');
+_putchar(hour / 10 + '0');
+_putchar(hour % 10 + '0');
+_putchar(':');
+_putchar(minute / 10 + '0');
+_putchar(minute % 10 + '0');
+_putchar('\n');
 }
 }
