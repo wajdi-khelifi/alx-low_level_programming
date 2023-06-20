cb62dce67dbaa9b8708401a6f09eeb1e9148058b
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-06-20 22:33:35 +0000

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,12 +10,16 @@ for (i = 0; i < 10; i++)
 for (j = 0; j < 10; j++)
 {
 t = i * j;
-if (t < 10)
+if (j == 0)
 {
 _putchar(t + '0');
+}
+if (j != 0 && t < 10)
+{
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
+_putchar(t + '0');
 }
 else if (t >= 10)
 {
