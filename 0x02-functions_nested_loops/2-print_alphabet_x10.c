71d7c3b454ff71d9cb4e459a2ac35d3b2fa49e07
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-06-20 15:25:06 +0000

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,13 +4,12 @@
 */
 void print_alphabet_x10(void)
 {
+int i;
 char alp;
-for (int i = 0; i < 10; i++)
+while (i++ < 10)
 {
 for (alp = 'a'; alp <= 'z'; alp++)
-{
 _putchar(alp);
-}
 _putchar('\n');
 }
 }
