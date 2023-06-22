2b88acdaa1e535c601d7306ed4dc7284c70a1d74
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-06-22 11:25:59 +0000

    Isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isupper - checks for uppercase character
+ * @c: The character to check
+ *
+ * Return: 1 if c is uppercase, return 0 otherwise
+ */
+int _isdigit(int c)
+{
+if (isdigit(c))
+{
+return (1);
+}
+else
+return (0);
+}i
