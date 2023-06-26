f2251a66a600f7fcd34480c7c78aa69e0da72ea4
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-06-26 20:22:21 +0000

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,53 +1,27 @@
 #include "main.h"
+
 /**
  * _atoi - Converts a string to an integer.
- * @s: The input string to convert.
+ * @s: The string to be converted.
  *
- * Return: The converted integer value.
- *
- * Description: This function converts a string to an integer, taking into
- * account any preceding - and + signs. If there are no numbers in the string,
- * it returns 0. The function does not use long, declare new variables of type
- * array, hard-code special values, and ensures it does not cause signed
- * integer overflow.
+ * Return: The integer value of the converted string.
  */
 int _atoi(char *s)
 {
-	int result = 0;
 	int sign = 1;
-	bool digitFound = false;
-    
-    while (*s != '\0')
-    {
-	    if (*s == '-' && !digitFound)
-	{
-		sign = -1;
-	}
-	else if (*s == '+' && !digitFound)
-	{
-		sign = 1;
-	}
-	else if (*s >= '0' && *s <= '9')
-	{
-		digitFound = true;
-	    result = result * 10 + (*s - '0');
-	    if (result < 0)
-	    {
-		    if (sign == 1)
-		{
-			return (-1);
-		}
-		else
-		{
-			return (0);
-		}
-	    }
-	}
-	else if (digitFound)
-	{
-		break;
-	}
-	s++;
-    }
-    return (sign * result);
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * sign);
 }
