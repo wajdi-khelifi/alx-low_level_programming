443bde89c799d786ff6f46ddf7a37e48e0e038f7
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-06-26 13:02:18 +0000

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strlen - check the code for Holberton School students.
+ * @s: declaration of *s and paramters for the function _strlen
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	size_t strlen(char *s);
+}
