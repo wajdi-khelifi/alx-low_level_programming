0fbb58110f1f9ab420e9d80aa6eaf99eaaa8f87f
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-06-29 10:32:56 +0000

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* *_strcat - Concatenates two strings.
+* @dest: Pointer to the destination string.
+* @src: Pointer to the source string.
+* Return: Pointer to the resulting string @dest.
+*/
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
