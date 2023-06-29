3111b4dcba3d74eeba58280a6d5263db8de739fd
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-06-29 12:53:00 +0000

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *string_toupper - Converts all lowercase letters of a string to uppercase.
+ * @str: The string to be modified.
+ *
+ * Return: A pointer to the modified string.
+ */
+char *string_toupper(char *)
+{
+	char *conv = str;
+
+	while (*conv != '0')
+	{
+		*conv = toupper(*conv);
+		conv++;
+	}
+	return (str);
+}
