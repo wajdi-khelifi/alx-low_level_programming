20d7082e8321cf6bf0a217e7ad4d5d881d6fba93
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-06-29 11:12:34 +0000

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,12 +3,11 @@
 * *_strncat - Concatenates two strings.
 * @dest: Pointer to the destination string.
 * @src: Pointer to the source string.
-* @n: An integer variable used to represent a numeric value
+* @n: Maximum number of characters to be concatenated.
 * Return: Pointer to the resulting string @dest.
 */
 char *_strncat(char *dest, char *src, int n)
 {
-	strcat(dest, src);
 	dest[n] = '\0';
-	return (dest);
+	return (strcat(dest, src));
 }
