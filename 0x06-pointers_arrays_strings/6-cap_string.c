f280bbe4ac5983f423899fbef5791d53e3db6334
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-06-29 14:08:46 +0000

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,26 +3,33 @@
  *cap_string - capitalizes every first letter of a word in a string.
  *separators of words are:  space, tabulation,
  * new line, ,, ;, ., !, ?, ", (, ), {, and }.
- *@str: pointer to string.
+ *@s: pointer to string.
  *
- *Return: pointer to str.
+ *Return: pointer to s.
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int cap = 1;
+	int count;
 
-	while (*str != '\0')
+	count = 0;
+	while (s[count] != '\0')
 	{
-	if (strchr(" \t\n,;.!?\"(){}", *str) != NULL)
-	{
-		cap = 1;
+		if (s[0] >= 97 && s[0] <= 122)
+		{
+			s[0] = s[0] - 32;
+		}
+		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+		    || s[count] == ',' || s[count] == ';' || s[count] == '.'
+		    || s[count] == '.' || s[count] == '!' || s[count] == '?'
+		    || s[count] == '"' || s[count] == '(' || s[count] == ')'
+		    || s[count] == '{' || s[count] == '}')
+		{
+			if (s[count + 1] >= 97 && s[count + 1] <= 122)
+			{
+				s[count + 1] = s[count + 1] - 32;
+			}
+		}
+		count++;
 	}
-	else if (cap && isalpha(*str))
-	{
-		*str = toupper(*str);
-		cap = 0;
-	}
-	str++;
-	}
-	return (str);
+	return (s);
 }
