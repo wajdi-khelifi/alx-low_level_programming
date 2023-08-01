540951c15adfad004947c8fbbf638c772ad1b91f
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-08-01 17:05:55 +0000

    function that prints a listint_t linked list (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -22,8 +22,8 @@ const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 	}
 	for (i = 0; i < size - 1; i++)
 		newlist[i] = list[i];
-		newlist[i] = new;
-		free(list);
+	newlist[i] = new;
+	free(list);
 	return (newlist);
 }
 
